43444668fe0e4a0a9f0930ce1a3116f86b15dda9
Cydney Vicentina <cjvicentinaucla@gmail.com>
2020-11-24 21:23:24 -0500

    fixing back button

--- assignment_8/src/script.js
+++ assignment_8/src/script.js
@@ -2,11 +2,12 @@
 // initialize jQuery
 jQuery (function() {
 	// for the process book folder, on click, change the html of the popup window
-	$("#process-books").on("click",function pb() {
-		// html for the popup window here
-		let pw = `
+
+    let pb0 = `<div class = "pb pb-1" style = "object-fit: cover;"><img class = "pop-up-image" src = "images/p1-pb.PNG" alt = "Process Book 1"><h5 class = "pop-up-text">UI Website Redesign</h5></div><div class = "pb pb-2"><img class = "pop-up-image" src = "images/p2-pb.PNG" alt = "Process Book 2"><h5 class = "pop-up-text">Lamp Control Redesign</h5></div><div class = "pb pb-3"><img class = "pop-up-image" src = "images/p3-pb.PNG" alt = "Process Book 3"><h5 class = "pop-up-text">Adidas Dashboard Design</h5></div><script src = "src/process-books.js"></script>`;
+
+	let pw = `
 		<div class = "window">
-        <div class="window-row">
+        <div class="window-row window-top">
           CMU Process Books
           <button id = "pb-back-button" class = "back-button" style = "float: left"><</button>
           <button class = "exit-button" style = "float: right">X</button>
@@ -17,18 +18,7 @@ jQuery (function() {
         </div>
         <div class="window-row" style = "padding: 0; padding-bottom: 52px">
           <div class="window-main-row">
-            <div class = "pb pb-1" style = "object-fit: cover;">
-              <img class = "pop-up-image" src = "images/p1-pb.PNG" alt = "Process Book 1">
-              <h5 class = "pop-up-text">UI Website Redesign</h5>
-            </div>
-            <div class = "pb pb-2">
-              <img class = "pop-up-image" src = "images/p2-pb.PNG" alt = "Process Book 2">
-              <h5 class = "pop-up-text">Lamp Control Redesign</h5>
-            </div>
-            <div class = "pb pb-3">
-              <img class = "pop-up-image" src = "images/p3-pb.PNG" alt = "Process Book 3">
-              <h5 class = "pop-up-text">Adidas Dashboard Design</h5>
-            </div>
+            ${pb0}
           </div>
         </div>
       </div>
@@ -39,32 +29,28 @@ jQuery (function() {
             $("#popup-window").html(noo);
             $(".main-window-mobile").html(noo);
           })
-          $(".pb-1").on("click", function() {
-            let pb1 = '<div class = "pdf-file"><iframe class = "frames" src="media/p1-pb.pdf" style = "overflow: auto"></iframe></div>';
-            $(".window-main-row").html(pb1);
-            $(".back-button").css('visibility','visible');
-          })
-          $(".pb-2").on("click", function() {
-            let pb2 = '<div class = "pdf-file"><iframe class = "frames" src="media/p2-pb.pdf" style = "overflow: auto"></iframe></div>';
-            $(".window-main-row").html(pb2);
-            $(".back-button").css('visibility','visible');
-          })
-          $(".pb-3").on("click", function() {
-            let pb3 = '<div class = "pdf-file"><iframe class = "frames" src="media/p3-pb.pdf" style = "overflow: auto"></iframe></div>';
-            $(".window-main-row").html(pb3);
-            $(".back-button").css('visibility','visible');
+          $(".back-button").on("click", function() {
+          	$(".window-main-row").html('${pb0}');
+          	$(".back-button").css('visibility','hidden');
           })
         })
       </script>
       `;
+
+	$("#process-books").on("click",function pb() {
+		// html for the popup window here
 		// change the html here
 		$("#popup-window").html(pw);
 		$(".main-window-mobile").html(pw);
 	})
 
+
+
 	// for the coding projects folder, on click, change the html of the popup window
 	$("#coding-projects").on("click",function cp() {
 		// html for the popup window here
+		let pb00 = `<div class = "pb pb-1" style = "object-fit: cover;"><img class = "pop-up-image" src = "images/battleship.PNG" alt = "Battleship Code"><h5 class = "pop-up-text">Play Battleship With R!</h5></div><div class = "pb pb-2"><img class = "pop-up-image" src = "images/R-logo.png" alt = "R Logo for Self-Written Package"><h5 class = "pop-up-text">Self-Written R Package</h5></div>`;
+
 		let pw = `
 		<div class = "window">
         <div class="window-row">
@@ -78,14 +64,7 @@ jQuery (function() {
         </div>
         <div class="window-row" style = "padding: 0; padding-bottom: 52px">
           <div class="window-main-row">
-            <div class = "pb pb-1" style = "object-fit: cover;">
-              <img class = "pop-up-image" src = "images/battleship.PNG" alt = "Battleship Code">
-              <h5 class = "pop-up-text">Play Battleship With R!</h5>
-            </div>
-            <div class = "pb pb-2">
-              <img class = "pop-up-image" src = "images/R-logo.png" alt = "R Logo for Self-Written Package">
-              <h5 class = "pop-up-text">Self-Written R Package</h5>
-            </div>
+          	${pb00}
           </div>
         </div>
       </div>
@@ -105,6 +84,10 @@ jQuery (function() {
             let pb2 = '<div class = "pdf-file"><iframe class = "frames" src="media/r-package.pdf" style = "overflow: auto"></iframe></div>';
             $(".window-main-row").html(pb2);
             $(".back-button").css('visibility','visible');
+          })
+          $(".back-button").on("click", function() {
+          	$(".window-main-row").html('${pb00}');
+          	$(".back-button").css('visibility','hidden');
           })
         })
       </script>
@@ -293,17 +276,38 @@ jQuery (function() {
     //})
 	$("#about-me-button").on("click",function() {
 		let am = `
-		<div class = "about-me-labels">Resume</div>
-    	<div class = "about-me-labels">Contact Me</div>
-    	<div class = "about-me-labels">Random Facts!</div>
+		<div id = 'am-resume' class = "about-me-labels" style = "cursor: pointer;">Resume</div>
+    	<div id = 'am-contact' class = "about-me-labels" style = "cursor: pointer;">Contact Me</div>
+    	<div id = 'am-fun' class = "about-me-labels" style = "cursor: pointer;">Fun Facts!</div>
+
+    	<script>
+    		$("#am-resume").on("click", function() {
+    			let amr = '<div class = "about-me-file">hello resume</div>';
+    			$(".about-me-main").html(amr);
+    		})
+    		$("#am-contact").on("click", function() {
+    			let amc = '<div class = "about-me-file">hello contact</div>';
+    			$(".about-me-main").html(amc);
+    		})
+    		$("#am-fun").on("click", function() {
+    			let amf = '<div class = "about-me-file">hello fun</div>';
+    			$(".about-me-main").html(amf);
+    		})
+    	</script>
 		`;
 		$(".about-me-tabs").html(am);
+		$(".about-me-tabs").css('z-index', '101');
+		$(".about-me-main").css('z-index', '100');
 		$(".invisible-div").css('width','100vw');
 		$(".invisible-div").css('height','100vh');
 	})
 	$(".invisible-div").on("click", function() {
 		let ind = '';
 		$(".about-me-tabs").html(ind);
+		$(".about-me-main").html(ind);
+		$("#am-resume").html(ind);
+		$("#am-contact").html(ind);
+		$("#am-fun").html(ind);
 		$(".invisible-div").css('width','0');
 		$(".invisible-div").css('height','0');
 	})
